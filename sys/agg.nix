c90accf8a9ff80af5bc855641a4b230c2aa01f8e
Brian <brianbraden@proton.me>
2026-04-25 12:47:27 +0900

    added modules and bundles

--- sys/agg.nix
+++ sys/agg.nix
@@ -2,22 +2,26 @@
 let
   bundlesDir = ../sys/bundles;
   modulesDir = ../sys/modules;
-  bundle = name: bundlesDir + "/${name}.nix"; =  ../sys/bundles;
-  module = name: modulesDir + "/${name}.nix"; =  ../sys/modules;
+  bundle = name: bundlesDir + "/${name}.nix";
+  module = name: modulesDir + "/${name}.nix";
 in
 {
-  inputs = [
-      global.nix
-      (bundle + "core")
-      (bundle + "audio")
-      (bundle + "audiotools")
-      (bundle + "hyprland")
-      (bundle + "locale")
-      (bundle + "music")
-      (bundle + "networking")
-      (bundle + "smartcard")
-      (bundle + "video")
-      (module + "podman")
-      (module + "qemu")
-    ]
+  imports = [
+      ./global.nix
+      (bundle "audio")
+      (bundle "audiotools")
+      (bundle "core")
+      (bundle "hyprland")
+      (bundle "locale")
+      (bundle "music")
+      (bundle "networking")
+      (bundle "smartcard")
+      (bundle "video")
+      (bundle "virtualization")
+      (module "fprint")
+      (module "hyprlock")
+      (module "mullvad")
+      (module "podman")
+      (module "qemu")
+    ];
 }
