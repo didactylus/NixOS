8d91ace169995c264f58ff0a93df9a27ea90b1d9
Brian <brianbraden@proton.me>
2026-06-18 14:17:01 +0900

    removed submodules and created a basic networking submodule

--- sys/networking.nix
+++ sys/networking.nix
@@ -1,24 +1,23 @@
 { config, lib, pkgs, ... }:
 
 let
-  cfg = config.nyx.bundles.networking;
+  cfg = config.nyx.networking;
 in
 
 {
-  options.nyx.bundles.networking = {
+  options.nyx.networking = {
     enable = lib.mkEnableOption "enable networking";
-    wifi = lib.mkEnableOption "enable wifi";
-    wwan = lib.mkEnableOption "enable wwan";
-    bt = lib.mkEnableOption "enable bluetooth";
   };
 
-  config = lib.mkIf cfg.enable {
-    networking.networkmanager.enable = true;
-    networking.firewall.enable = true;
-    services.resolved.enable = true;
+  config = lib.mkMerge [
 
-    environment.systemPackages = with pkgs; [
-      
-    ];
-  };
+    (lib.mkIf cfg.enable {
+      networking.networkmanager.enable = true;
+      networking.firewall.enable = true;
+      services.resolved.enable = true;
+      programs.mtr.enable = true;
+
+    })
+
+  ];
 }
