c1178b0dc1c4e798300b41abf8d074953333c085
Brian <brianbraden@proton.me>
2026-06-18 14:14:45 +0900

    beets is insecure

--- sys/media.nix
+++ sys/media.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }:
 
 let
-  cfg = config.nyx.bundle.media;
+  cfg = config.nyx.media;
 in
 
 {
-  options.nyx.bundle.media = {
+  options.nyx.media = {
     music.enable = lib.mkEnableOption "enable music";
     music.tools.enable = lib.mkEnableOption "enable music tools";
 
@@ -14,30 +14,35 @@ in
   };
 
   config = lib.mkMerge [
+
     (lib.mkIf cfg.music.enable {
       environment.systemPackages = with pkgs; [
         mpd
         rmpc
       ];
     })
+
     (lib.mkIf cfg.music.tools.enable {
       environment.systemPackages = with pkgs; [
         lrcget
-        beets
+        #beets
         chromaprint
         yt-dlp
         wrtag
       ];
     })
+
     (lib.mkIf cfg.video.enable {
       environment.systemPackages = with pkgs; [
         mpv
       ];
     })
+
     (lib.mkIf cfg.video.tools.enable {
       environment.systemPackages = with pkgs; [
         ffmpeg
       ];
     })
+
   ];
-};
+}
