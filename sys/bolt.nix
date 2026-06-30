eb9c3a439f25d37d7657691904e763914e531ce7
didactylus <brianbraden@proton.me>
2026-06-30 10:51:43 +0900

    fixed syntax

--- sys/bolt.nix
+++ sys/bolt.nix
@@ -1,16 +1,25 @@
-{ config, lib, pkgs }:
+{ config, lib, pkgs, ... }:
 
 let
   cfg = config.nyx.bolt;
 in
-
 {
   options.nyx.bolt = {
     enable = lib.mkEnableOption "enable USB4/Thunderbolt";
+
+    authorizeGroup = lib.mkOption {
+      type = lib.types.str;
+      default = "wheel";
+    };
+
+    passwordlessAuthorization = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
   };
 
-  config = MkMerge [
-    (lib.MkIf cfg.enable {
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
       security.polkit.enable = true;
       services.hardware.bolt.enable = true;
 
@@ -24,20 +33,19 @@ in
         bolt
       ];
 
-      security.polkit.extraConfig =
-      ''
-      polkit.addRule(function(action, subject) {
-        if (
-          subject.isInGroup("${cfg.authorizeGroup}") &&
+      security.polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("${cfg.authorizeGroup}") &&
             (
               action.id == "org.freedesktop.bolt.authorize" ||
               action.id == "org.freedesktop.bolt.enroll"
             )
           ) {
-          return polkit.Result.${if cfg.passwordlessAuthorization the "YES" else "AUTH_ADMIN"};
+            return polkit.Result.${if cfg.passwordlessAuthorization then "YES" else "AUTH_ADMIN"};
           }
         });
       '';
-    }
-  ]
+    })
+  ];
 }
